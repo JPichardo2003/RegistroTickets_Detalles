5b203ae24703e578625f83b315f63a87086f2acd
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-20 15:09:07 -0400

    Creando Modelos: Clientes, Tickets, TicketsDetalles

---
+++ Shared/Models/Tickets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroTickets_Detalles.Shared.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Descripción { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        [StringLength(30, ErrorMessage = "No puede exceder los 30 Caracteres")]
+        public string? SolicitadoPor { get; set; }
+
+        [ForeignKey("TicketId")]
+        public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
+    }
+}
---
+++ Shared/Models/TicketsDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroTickets_Detalles.Shared.Models
+{
+    public class TicketsDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Este campo no acepta digitos")]
+        public string? Emisor { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string? Mensaje { get; set; }
+
+
+    }
+}
