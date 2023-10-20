85a7f21100c3f729ba3cd50a2695e03a5f3db375
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-20 15:25:33 -0400

    Añadiendo contexto

---
+++ Server/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTickets_Detalles.Shared.Models;
+
+namespace RegistroTickets_Detalles.Server.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+    }
+}
