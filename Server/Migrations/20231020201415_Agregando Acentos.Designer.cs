1587798cf91f4c66d8f329cf01e19cf4020eff14
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-20 16:44:57 -0400

    Nueva Migracion debido a cambios en el Models

--- Server/Migrations/20231020201415_Agregando Acentos.Designer.cs
+++ Server/Migrations/20231020201415_Agregando Acentos.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroTickets_Detalles.Server.DAL;
 
@@ -10,9 +11,11 @@ using RegistroTickets_Detalles.Server.DAL;
 namespace RegistroTickets_Detalles.Server.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20231020201415_Agregando Acentos")]
+    partial class AgregandoAcentos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.12");
---
+++ Server/Migrations/20231020201415_Agregando Acentos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTickets_Detalles.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoAcentos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Telefono",
+                table: "Clientes",
+                newName: "Teléfono");
+
+            migrationBuilder.RenameColumn(
+                name: "Nombres",
+                table: "Clientes",
+                newName: "Nombre");
+
+            migrationBuilder.RenameColumn(
+                name: "Direccion",
+                table: "Clientes",
+                newName: "Dirección");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Teléfono",
+                table: "Clientes",
+                newName: "Telefono");
+
+            migrationBuilder.RenameColumn(
+                name: "Nombre",
+                table: "Clientes",
+                newName: "Nombres");
+
+            migrationBuilder.RenameColumn(
+                name: "Dirección",
+                table: "Clientes",
+                newName: "Direccion");
+        }
+    }
+}
