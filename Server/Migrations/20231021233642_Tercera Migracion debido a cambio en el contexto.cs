2f462ab30e2c0bc28e4f94e0040c4c0ebbd33f80
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-21 19:39:48 -0400

    Nueva Migracion y Base de datos debido a cambios en el contexto

---
+++ Server/Migrations/20231021233642_Tercera Migracion debido a cambio en el contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTickets_Detalles.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class TerceraMigraciondebidoacambioenelcontexto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
