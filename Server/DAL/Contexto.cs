424b4876f9647c5a011cec0bbac0d725f1a5eeb2
JPichardo2003 <julio_pichardo@ucne.edu.do>
2023-10-21 19:39:19 -0400

    Añadiendo TicketsDetalle al contexto

--- Server/DAL/Contexto.cs
+++ Server/DAL/Contexto.cs
@@ -8,5 +8,6 @@ namespace RegistroTickets_Detalles.Server.DAL
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
         public DbSet<Tickets> Tickets { get; set; }
         public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<TicketsDetalle> TicketsDetalle { get; set; }
     }
 }
